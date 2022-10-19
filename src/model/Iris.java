f9ed7a4e832fd2644b74f8d9f54c863f2fc95dd6
Simon Bocquet <simon.bocquet.etu@univ-lille.fr>
2022-10-19 17:16:00 +0200

    Iris fait

--- src/model/Iris.java
+++ src/model/Iris.java
@@ -1,5 +1,31 @@
 package model;
 
-public class Iris {
+import com.opencsv.bean.CsvBindByName;
+
+import util.IColumn;
+import util.IPoint;
+import util.IrisVariety;
+
+public class Iris implements IPoint {
+	@CsvBindByName(column = "sepai_length")
+	private double sepaiLength;
+	@CsvBindByName(column = "sepai_width")
+	private double sepaiWidth;
+	@CsvBindByName(column = "petal_length")
+	private double petalLength;
+	@CsvBindByName(column = "petal_width")
+	private double petalWidth;
+	@CsvBindByName(column = "variety")
+	private IrisVariety variety;
+	
+	@Override
+	public Object getValue(IColumn col) {
+		return null;
+	}
+
+	@Override
+	public double getNormalizedValue(IColumn xcol) {
+		return xcol.getNormalizedValue(this);
+	}
 
 }
