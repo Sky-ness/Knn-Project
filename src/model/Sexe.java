c8eed62871ccde6540d5f7b19e0d647a31e6c471
Antonin Duvauchel <antonin.duvauchel.etu@univ-lille.fr>
2022-10-19 17:20:25 +0200

    Titanic finis

---
+++ src/model/Sexe.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum Sexe {
+	MALE,FEMALE
+}
--- src/model/Titanic.java
+++ src/model/Titanic.java
@@ -1,5 +1,44 @@
 package model;
 
-public class Titanic {
+import com.opencsv.bean.CsvBindByName;
+
+import util.IColumn;
+import util.IPoint;
+
+public class Titanic implements IPoint{
+	@CsvBindByName(column = "PassengerId")
+	private int passengerId;
+	@CsvBindByName(column = "Survived")
+	private int survived;
+	@CsvBindByName(column = "Pclass")
+	private int Pclass;
+	@CsvBindByName(column = "Name")
+	private String name;
+	@CsvBindByName(column = "Sex")
+	private Sexe sex;
+	@CsvBindByName(column = "Age")
+	private int age;
+	@CsvBindByName(column = "SibSp")
+	private int sibSp;
+	@CsvBindByName(column = "Parch")
+	private int parch;
+	@CsvBindByName(column = "Ticket")
+	private String ticket;
+	@CsvBindByName(column = "Fare")
+	private double fare;
+	@CsvBindByName(column = "Cabin")
+	private String cabin;
+	@CsvBindByName(column = "Embarked")
+	private char embarked;
+	
+	@Override
+	public Object getValue(IColumn col) {
+		return null;
+	}
+	@Override
+	public double getNormalizedValue(IColumn xcol) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
