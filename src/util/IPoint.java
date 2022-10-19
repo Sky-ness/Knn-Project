a6eb6c519218a15c26af5536a5f23c9326173fd8
Simon Bocquet <simon.bocquet.etu@univ-lille.fr>
2022-10-19 16:04:01 +0200

    ajout Interface fournis

---
+++ src/util/IPoint.java
@@ -0,0 +1,22 @@
+package util;
+
+/**
+* Decrit un Point (ou donnee, ou ligne) dans un DataSet.
+*/
+public interface IPoint {
+/**
+* Retourne la valeur de ce point pour la colonne en parametre.
+*
+* Note, on aurait pu utiliser une interface generique (parametree avec
+* un type), mais cela complique significativement d'autres parties
+* du code.
+*/
+public Object getValue(IColumn col);
+/**
+* Retourne la valeur de ce point normalisee pour la colonne en parametre.
+*
+* La normalisation se fait avec le <i>normaliseur</i> de la colonne.
+* Si la colonne n'est pas normalisable, le comportement n'est pas defini.
+*/
+public double getNormalizedValue(IColumn xcol);
+}
