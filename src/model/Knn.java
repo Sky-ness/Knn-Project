8c748304c932fc2e66282a982abfafe51e0fc3ae
Raymond Khampan <raymond.khampan.etu@univ-lille.fr>
2022-11-14 11:33:43 +0100

    TestNormaliseBoolean + cleancode

--- src/model/Knn.java
+++ src/model/Knn.java
@@ -11,16 +11,6 @@ import utils.IPoint;
 
 public class Knn extends Classifieur{
 	
-
-//	@Override
-//	public List<IPoint> neighbor(int k, IPoint point, IDistance distance,List<IPoint> list,IColumn c1,IColumn c2) {
-//		test.sort((i1,i2) -> Double.compare(distance.distanceManhattan(i1,point,c1,c2), distance.distanceManhattan(i2, point,c1,c2)));
-//		for(int i = 0; i < k; i++) {
-//			list.add(test.get(i));
-//		}
-//		return list;
-//	}
-	
 	@Override
 	public List<IPoint> neighbor(int k, IPoint point, IDistance distance,List<IPoint> list,List<IColumn> c1) {
 		List<IPoint> test= new ArrayList<IPoint>();
