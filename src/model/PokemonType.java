041a998cd400e243240b5f741ac03659d56f3885
Simon Bocquet <simon.bocquet.etu@univ-lille.fr>
2022-11-17 16:48:41 +0100

    pokemontype normalizer

--- src/model/PokemonType.java
+++ src/model/PokemonType.java
@@ -1,19 +1,5 @@
 package model;
 
 public enum PokemonType {
-	BUG(1),DARK(2),DRAGON(3),ELECTRIC(4),FAIRY(5),
-	FIGHTING(6),FIRE(7),FLYING(8),GHOHST(9),GRASS(10),
-	GROUND(11),ICE(12),NORMAL(13),POISON(14),PSYCHIC(15),
-	ROCK(16),STEEL(17),WATER(18),GHOST(19);
-
-	double value;
-	
-	PokemonType(int i) {
-		value = i;
-	}
-
-	double getValue() {
-		return value;
-	}
-	
+	BUG,DARK,DRAGON,ELECTRIC,FAIRY,FIGHTING,FIRE,FLYING,GHOHST,GRASS,GROUND,ICE,NORMAL,POISON,PSYCHIC,ROCK,STEEL,WATER,GHOST;
 }
--- src/model/Pokemon_Type_Normalizer.java
+++ src/model/Pokemon_Type_Normalizer.java
@@ -4,13 +4,14 @@ import utils.IValueNormalizer;
 
 public class Pokemon_Type_Normalizer implements IValueNormalizer{
 	
-	private static final int NB_TYPES = 19;
+	// nombre de types-1
+	private static final int NB_TYPES = 18;
 	
 	@Override
 	public double normalize(Object value) {
 		if(PokemonType.class.equals(value.getClass())) {
 			PokemonType pt = (PokemonType) value;
-			return pt.getValue()/NB_TYPES;
+			return pt.ordinal()/NB_TYPES;
 		}
 			
 		return -1;
@@ -20,6 +21,7 @@ public class Pokemon_Type_Normalizer implements IValueNormalizer{
 	public Object denormalize(double value) {
 		if(value < 0 || value > 1)
 			return null;
+		PokemonType.BUG.ordinal();
 		return PokemonType.values()[(int) (value * NB_TYPES)];
 	}
 
