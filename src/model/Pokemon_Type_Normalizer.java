71812b29bb15c16d3f149d526ffa97be0b1d9800
Hocine Aliouat <hocine.aliouat.etu@univ-lille.fr>
2022-11-17 16:28:48 +0100

    pokemon type normalizer

--- src/model/Pokemon_Type_Normalizer.java
+++ src/model/Pokemon_Type_Normalizer.java
@@ -5,16 +5,41 @@ import utils.IValueNormalizer;
 public class Pokemon_Type_Normalizer implements IValueNormalizer{
 	
 	
+	protected Double[] createTab() {
+		PokemonType[] PkType = PokemonType.values();
+		Double[] tab = new Double[PkType.length];
+		for(int i = 0; i<tab.length;i++) {
+			tab[i] = (double)i/(tab.length-1);
+		}
+		return tab;
+	}
+	
 	@Override
 	public double normalize(Object value) {
-		// TODO Auto-generated method stub
+		PokemonType[] PkType = PokemonType.values();
+		Double[] tab = createTab();
+		int i = 0;
+		for(PokemonType type : PkType){
+			if(value.equals(type)) {
+				return tab[i];
+			}
+			i++;
+		}
+		
 		return 0;
 	}
 
 	@Override
 	public Object denormalize(double value) {
-		// TODO Auto-generated method stub
+		PokemonType[] PkType = PokemonType.values();
+		Double[] tab = createTab();
+		int i = 0;
+		for(double db : tab) {
+			if(value == db) {
+				return PkType[i];
+			}
+			i++;
+		}
 		return null;
 	}
-
 }
