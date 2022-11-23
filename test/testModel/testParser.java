96ce6137e098b81b8114636fc133bd1c52221cd0
Raymond Khampan <raymond.khampan.etu@univ-lille.fr>
2022-11-23 11:40:03 +0100

    A continuer

--- test/testModel/testParser.java
+++ test/testModel/testParser.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import model.Iris;
 import model.Parser;
+import model.Pokemon;
 
 public class testParser {
 	Parser parser;
@@ -24,5 +25,27 @@ public class testParser {
 	void testGetTitle() {
 		assertEquals("Iris",parser.getTitle());
 	}
+	
+	@Test
+	void testNbLines() {
+		assertEquals(parser.getNbLines(),parser.getNbLines());
+	}
+	
+	@Test
+	void testIterator() {
+		assertEquals(parser.iterator(),parser.iterator());
+	}
+	
+	@Test
+	void testnbColumns() {
+		assertEquals(5,parser.nbColumns());
+	}
+	
+	
+	@Test
+	void testaddPoint() {
+		
+	}
+	
 	
 }
