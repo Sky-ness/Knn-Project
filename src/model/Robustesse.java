c96cdbb90e7cab6f4e8ba3e04a98ff8ed9a6d610
Hocine Aliouat <hocine.aliouat.etu@univ-lille.fr>
2022-11-23 11:23:56 +0100

    Robustesse fini

--- src/model/Robustesse.java
+++ src/model/Robustesse.java
@@ -1,24 +1,22 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import utils.AbstractClassifier;
 import utils.IPoint;
 
 public class Robustesse {
 	
-	@SuppressWarnings("PMD.ExcessiveParameterList")
 	public double calc(DataSet ds, int k, AbstractClassifier classifier, Distance distance, Column col) {
 		Object value;
 		double i = 0.0;
 		List<IPoint> neighbor;
-		List<IPoint> list = ds.getListPoints();
-		System.out.println("1");
+		List<IPoint> list = init(ds);
 		for(int j = 0; j<list.size();j++) {
-			neighbor = classifier.neighbor(k,list.get(j), distance, ds.getListPoints(), ds.getListColumns());
-			System.out.println(j);
+			list = init(ds);
+			neighbor = classifier.neighbor(k,list.get(j), distance, list, ds.getListColumns());
 			value = classifier.classify(neighbor,col);
-			System.out.println("test : "+list.get(j).getValue(col) + " " + value);
+			list = init(ds);
 			if(value.equals(list.get(j).getValue(col))){
 				
 				i++;
@@ -27,11 +25,19 @@ public class Robustesse {
 		}
 		return (i+0.0)/(ds.getNbLines()+0.0)*100;
 	}
+	
+	public List<IPoint> init(DataSet ds){
+		List<IPoint> list = new ArrayList<IPoint>();
+		for(IPoint point : ds.getListPoints()) {
+			list.add(point);
+		}
+		return list;
+	}
 
 	public static void main(String[] args) {
 		Parser parser = new Parser();
-		parser.loadFromString("data/titanic.csv");
-		Column col = parser.getListColumns().get(11);
+		parser.loadFromString("data/pokemon_test.csv");
+		Column col = parser.getListColumns().get(12);
 		Robustesse rob = new Robustesse();
 		System.out.println("Pourcentage : " + rob.calc(parser.datas,5,new Knn(),new Distance(),col));
 		
