849ffd24ddeaabb478654e6e25d9015f4ab4396a
Raymond Khampan <raymond.khampan.etu@univ-lille.fr>
2022-11-30 16:09:51 +0100

    Titanic reglage

--- test/testModel/testTitanic.java
+++ test/testModel/testTitanic.java
@@ -14,7 +14,7 @@ class testTitanic {
 	
 	@Test
 	void testInstantationGeneriqueSansErreur() {
-		Titanic titanic = new Titanic(1,0,3,"Sir Faker",Sexe.MALE,26.0,1,0,"Goat",7.25,"y",Embarked.C);
+		Titanic titanic = new Titanic("1","0","3","Sir Faker","MALE","26.0","1","0","Goat","7.25","y","C");
 		assertEquals(1,titanic.getPassengerId());
 		assertEquals(0,titanic.getSurvived());
 		assertEquals(3,titanic.getPclass());
@@ -35,14 +35,14 @@ class testTitanic {
 		assertEquals(0,titanic.getPassengerId());
 		assertEquals(0,titanic.getSurvived());
 		assertEquals(0,titanic.getPclass());
-		assertEquals(null,titanic.getName());
+		assertEquals("",titanic.getName());
 		assertEquals(null,titanic.getSex());
 		assertEquals(0,titanic.getAge());
 		assertEquals(0,titanic.getSibSp());
 		assertEquals(0,titanic.getParch());
-		assertEquals(null,titanic.getTicket());
+		assertEquals("",titanic.getTicket());
 		assertEquals(0,titanic.getFare());
-		assertEquals(null,titanic.getCabin());
+		assertEquals("",titanic.getCabin());
 		assertEquals(null,titanic.getEmbarked());
 	}
 	
@@ -54,7 +54,7 @@ class testTitanic {
 	
 	@Test
 	void testToString() {
-		Titanic titanic = new Titanic(1,0,3,"Sir Faker",Sexe.MALE,26.0,1,0,"Goat",7.25,"y",Embarked.C);
+		Titanic titanic = new Titanic("1","0","3","Sir Faker","MALE","26.0","1","0","Goat","7.25","y","C");
 		String res = "Titanic [passengerId=1, survived=0, Pclass=3, name=Sir Faker, sex=MALE, age=26.0, sibSp=1, parch=0, ticket=Goat, fare=7.25, cabin=y, embarked=C]"
 ;		
 		assertEquals(res,titanic.toString());
